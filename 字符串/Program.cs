0250436f6e8a7687a5cf8294320c9d2ba79e692b
张文杰 <746342384@qq.com>
2021-12-28 21:55:01 +0800

    添加项目文件。

---
+++ 字符串/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Globalization;
+using System.Text;
+
+namespace 字符串
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            // var s = "www.devsiki.com";
+            // Console.WriteLine(s.Length);
+            // Console.WriteLine(s == "www.devsiki.com");
+            // s = "http://" + s;
+            // Console.WriteLine(s);
+            // Console.WriteLine(s[3]);
+            // Console.WriteLine(s.CompareTo("www.devsiki.com"));
+            // Console.WriteLine(s.CompareTo("devsiki"));
+            // Console.WriteLine(s.Replace(".", "-"));
+            // Console.WriteLine($"{1}+{2}+{3}");
+            // Console.WriteLine($"{100:C}");
+            // Console.WriteLine($"{0:F1}",100.555);
+            // Console.WriteLine($"{0:P1}",1.566);
+            //Console.WriteLine($"{DateTime.Now:yyyy-MM-dd HH:MM}");
+            // var stringBuilder = new StringBuilder("www.devsiki.com");
+            // stringBuilder.Append("aaa");
+            // Console.WriteLine(stringBuilder);
+            // stringBuilder.Insert(3, "aaa");
+            // Console.WriteLine(stringBuilder);
+            // stringBuilder.Remove(3,3);
+            // Console.WriteLine(stringBuilder);
+            // stringBuilder.Replace("a", "");
+            // Console.WriteLine(stringBuilder);
+        }
+    }
+}
---
+++ 正则表达式/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace 正则表达式
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var str1 = "23432n";
+            var str2 = "234234";
+            // Console.WriteLine( Regex.IsMatch(str1, @"\d\d\d\d\d\d"));
+            // Console.WriteLine( Regex.IsMatch(str2, @"\d\d\d\d\d\d"));
+            // Console.WriteLine( Regex.IsMatch(str1, @"\d*"));
+            // Console.WriteLine( Regex.IsMatch(str1, @"^\d*$"));
+            // Console.WriteLine( Regex.IsMatch(str2, @"^\d*$"));
+            // \d数字
+            // \w 大小写字母 0-9 _
+            // Console.WriteLine( Regex.IsMatch(str1, @"^\w*$"));
+            // Console.WriteLine( Regex.IsMatch(str2, @"^\w*$"));
+            // Console.WriteLine(Regex.IsMatch("a",@"[abcd]"));
+            // Console.WriteLine(Regex.IsMatch("e",@"[abcd]"));
+            // Console.WriteLine(Regex.IsMatch("e",@"[a-gx-z]"));
+            // Console.WriteLine(Regex.IsMatch("h",@"[A-H]"));
+            // Console.WriteLine(Regex.IsMatch("h",@"[^A-H]"));
+            //是否是合法标识符
+            // string re = @"^[a-zA-Z]\w*$";
+            // Console.WriteLine(Regex.IsMatch("abcd",re));
+            // Console.WriteLine(Regex.IsMatch("2sdf",re));
+            // Console.WriteLine(Regex.IsMatch("sdfsd4sdf",re));
+            // Console.WriteLine(Regex.IsMatch("_sdfadsf",re));
+            // Console.WriteLine(Regex.IsMatch("_2sdfsdf",re));
+            // Console.WriteLine(Regex.IsMatch("_2sdfsd3",re));
+        }
+    }
+}
