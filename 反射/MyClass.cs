a209e4a6728bba439a7994b085bcf74fd047e5c1
张文杰 <746342384@qq.com>
2021-12-29 23:03:40 +0800

    反射获取方法，并调用

--- 反射/MyClass.cs
+++ 反射/MyClass.cs
@@ -14,5 +14,20 @@ namespace 反射
         public int d;
 
         public string Name { get; set; }
+
+        public void MyMethod()
+        {
+            Console.WriteLine("这是一个方法");
+        }
+
+        public void MyMethod(string str)
+        {
+            Console.WriteLine("这是一个有参数的方法" + str);
+        }
+        public string MyMethod(string str1, string str2)
+        {
+            Console.WriteLine("这是一个有返回值参数的方法" + str1 + str2);
+            return str1 + str2;
+        }
     }
 }
--- 反射/Program.cs
+++ 反射/Program.cs
@@ -1,19 +1,16 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Reflection;
 
 namespace 反射
 {
     class Program
-    { 
+    {
         static void Main(string[] args)
         {
             //Type
-            Type t = typeof(MyClass);
-
+            //Type t = typeof(MyClass);
+            MyClass myClass = new MyClass();
+            Type t = myClass.GetType();
             Console.WriteLine(t.Name);
             Console.WriteLine(t.Namespace);
             Console.WriteLine(t.Assembly);
@@ -29,6 +26,33 @@ namespace 反射
             {
                 Console.WriteLine(item.Name);
             }
+
+            MethodInfo[] methods = t.GetMethods();
+            foreach (var item in methods)
+            {
+                Console.WriteLine(item.Name);
+            }
+            Object obj = Activator.CreateInstance(t);
+            MethodInfo method = t.GetMethod("MyMethod",
+                new Type[] { });
+            object[] parameters = null;
+            method?.Invoke(obj, parameters);
+            method = t.GetMethod("MyMethod",
+                new Type[]
+                {
+                    typeof(string)
+                });
+            parameters = new object[] { "hello world!" };
+            method?.Invoke(obj, parameters);
+
+            method = t.GetMethod("MyMethod", new Type[]
+                {
+                    typeof(string),
+                    typeof(string)
+                });
+            parameters = new object[] { "hello", " world!" };
+            var result = method?.Invoke(obj, parameters);
+            Console.WriteLine(result);
             Console.ReadLine();
         }
     }
