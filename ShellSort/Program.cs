3c0bdce4deb23866af51f126f4ade90fc4a9cbe5
Rirr <746342384@qq.com>
2022-01-25 17:59:58 +0800

    希尔排序

---
+++ ShellSort/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShellSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 5, 8, 6, 0, 9, 2, 4, 3, 5, 1 };
+            ShellSort(arr);
+            foreach (var item in arr)
+                Console.WriteLine(item);
+            Console.ReadLine();
+        }
+        static void ShellSort(int[] arr)
+        {
+            for (int i = arr.Length / 2; i > 0; i = i / 2)
+            {            
+                for (int k = i; k < arr.Length; k++)
+                {
+                    for (int j = k; j > 0; j--)
+                    {
+                        if (arr[j] < arr[j - 1])
+                        {
+                            (arr[j], arr[j - 1]) = (arr[j - 1], arr[j]);
+                        }
+                        else
+                        {
+                            break;
+                        }
+                    }
+                }
+
+            }
+        }
+    }
+}
